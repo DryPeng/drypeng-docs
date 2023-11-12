724a2a90efda349146a48d7c8ee3285743492c01
LOVAHE <lovahe@drypeng.io>
2023-11-12 05:18:19 +0000

    set up new env

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withMDX = require('@next/mdx')()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+
+module.exports = withMDX(nextConfig)
