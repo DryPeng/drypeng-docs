c9a9325968dca23b5c1c47629142aa3b3095e82f
LOVAHE <lovahe@drypeng.io>
2023-11-13 21:38:05 +0900

    add index page

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+function IndexPage() {
+    return (
+      <div>
+        <Head>
+          <title>My page title</title>
+        </Head>
+        <Head>
+        </Head>
+        <p>Hello world!</p>
+      </div>
+    )
+  }
+   
+  export default IndexPage
