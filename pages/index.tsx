d485e3d98d250814933216303e9d28a9bcbdc7dc
LOVAHE <lovahe@drypeng.io>
2023-11-19 12:40:03 +0900

    fix6

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 
 // components and styles
 import Header from '@components/header'
-import './app/home.css'
+import styles from './app/home.module.css'
 
 function IndexPage() {
     return (
