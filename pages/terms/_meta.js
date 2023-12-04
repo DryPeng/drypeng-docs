ae2936e57ac2ad84caa665e3a43260559b28a093
LOVAHE <lovahe@drypeng.io>
2023-12-04 12:06:13 +0900

    Delete pages/terms/_meta.js

--- pages/terms/_meta.js
+++
@@ -1,9 +0,0 @@
-const { createCatchAllMeta } = require('nextra/catch-all')
-import { useTerms } from "@/hooks";
-
-module.exports = async () => {
-  const { data } = await useTerms();
-  // FIXME: This is horrible but does the job
-  const pageList = data.map((item) => {return item.name + '.mdx'})
-  return createCatchAllMeta(pageList)
-};
